85f807e78c33adeabb539e9d0a204ab59c66e6ae
T35000241 <darlyn.vailatti@compasso.com.br>
2020-03-22 21:24:43 -0300

    Add exposeID for common entities

--- src/main/java/br/com/bempromotora/backend/configuration/RestDataConfig.java
+++ src/main/java/br/com/bempromotora/backend/configuration/RestDataConfig.java
@@ -1,6 +1,8 @@
 package br.com.bempromotora.backend.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
+import br.com.bempromotora.backend.domain.cliente.ClienteEntity;
+import br.com.bempromotora.backend.domain.cliente.ConvenioEntity;
+import br.com.bempromotora.backend.domain.proposta.PropostaEntity;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.core.mapping.RepositoryDetectionStrategy;
@@ -12,6 +14,9 @@ public class RestDataConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration repositoryRestConfiguration) {
         repositoryRestConfiguration.setRepositoryDetectionStrategy(RepositoryDetectionStrategy.RepositoryDetectionStrategies.ANNOTATED);
+        repositoryRestConfiguration.exposeIdsFor(ClienteEntity.class);
+        repositoryRestConfiguration.exposeIdsFor(PropostaEntity.class);
+        repositoryRestConfiguration.exposeIdsFor(ConvenioEntity.class);
     }
 
 }
